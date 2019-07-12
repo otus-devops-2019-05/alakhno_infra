d5e3859bc81e043b222d94998d77f3fe8393bb0c
Aleksei Lakhno <alakhno@gmail.com>
2019-07-12 12:26:17 +0300

    Перенести app.tf и db.tf в модули

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,12 @@
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable zone {
+  description = "Zone"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "app_external_ip" {
-  value = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
+  value = "${module.app.app_external_ip}"
 }
 
 output "db_external_ip" {
-  value = "${google_compute_instance.db.network_interface.0.access_config.0.nat_ip}"
+  value = "${module.db.db_external_ip}"
 }
